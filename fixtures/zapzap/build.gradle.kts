30f3efe8f7885750ad247afb184f8f69c4894ead
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-01 14:28:38 -0300

    fix(zapzap): preload isolated transitive versions

--- fixtures/zapzap/build.gradle.kts
+++ fixtures/zapzap/build.gradle.kts
@@ -5,3 +5,22 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 }
+
+val offlineExactArtifacts by configurations.creating {
+    isCanBeConsumed = false
+    isCanBeResolved = true
+}
+
+dependencies {
+    offlineExactArtifacts("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    offlineExactArtifacts("androidx.annotation:annotation-jvm:1.8.1")
+}
+
+tasks.register("hydrateOfflineExactArtifacts") {
+    inputs.files(offlineExactArtifacts)
+    doLast {
+        offlineExactArtifacts.files.forEach { file ->
+            check(file.isFile) { "Missing offline artifact: ${file.name}" }
+        }
+    }
+}
