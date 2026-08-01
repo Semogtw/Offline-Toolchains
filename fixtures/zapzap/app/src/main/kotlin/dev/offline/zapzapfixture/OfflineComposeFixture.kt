ce3ecbef5cadb92b7802f0152a326f2a16426f01
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-01 14:28:00 -0300

    test(zapzap): compile Compose fixture source

---
+++ fixtures/zapzap/app/src/main/kotlin/dev/offline/zapzapfixture/OfflineComposeFixture.kt
@@ -0,0 +1,9 @@
+package dev.offline.zapzapfixture
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun OfflineComposeFixture() {
+    Text(OfflineJvmFixture.VALUE)
+}
